e0809121ab07d0105896430254bcebb86285907f
Hannes Alm <hannes@almar.se>
2019-01-18 09:49:29 +0100

    updated server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(secure);
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
+// Catch all routes and redirect to the index file
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 // Log to feedback that this is actually running
